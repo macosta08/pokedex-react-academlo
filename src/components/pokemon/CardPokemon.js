d4be05f5a9d670096de6b058eb82b15fd1079e10
Mariangel <mariangelacosta.88@gmail.com>
2021-03-04 15:32:30 -0500

    card superior

--- src/components/pokemon/CardPokemon.js
+++ src/components/pokemon/CardPokemon.js
@@ -1,23 +1,31 @@
 import React from "react";
 import { BaseStats } from "../baseStats/BaseStats";
 import { TypeIconPoke } from "../typeIconPoke/TypeIconPoke";
-
+import "./cardPokemon.css";
 export const CardPokemon = ({ infoPoke, handleReturn }) => {
   return (
-    <div>
+    <>
       {infoPoke && (
-        <div>
-          <h1>{infoPoke.name}</h1>
-          <h2>#0{infoPoke.id}</h2>
-          <img
-            src={infoPoke.sprites.other["official-artwork"].front_default}
-            alt={infoPoke.name}
-          />
-          <TypeIconPoke types={infoPoke.types} />
-          <BaseStats stats={infoPoke.stats} />
-          <button onClick={handleReturn}>Return</button>
+        <div className="d-flex  bd-highlight container-card">
+          <div class="p-2 flex-fill bd-highlight">
+            <div className=" imgId">#{infoPoke.id}</div>
+            <div class="d-flex justify-content-center back-center">
+              <h1>{infoPoke.name}</h1>
+              <img
+                src={infoPoke.sprites.other["official-artwork"].front_default}
+                alt={infoPoke.name}
+              />
+            </div>
+          </div>
+          <div className="p-2 flex-fill bd-highlight">
+            <div>
+              <TypeIconPoke types={infoPoke.types} />
+              <BaseStats stats={infoPoke.stats} />
+              <button onClick={handleReturn}>Return</button>
+            </div>
+          </div>
         </div>
       )}
-    </div>
+    </>
   );
 };
